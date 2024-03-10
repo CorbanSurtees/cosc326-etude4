fdeca96314d75ed6834c70031361cc3d66db00b5
Corban Surtees <surco973@student.otago.ac.nz>
2024-03-10 21:37:57 +1300

    add example instructions

--- script.js
+++ script.js
@@ -17,8 +17,7 @@ function generateSquares() {
 
     console.log(inputValues)
     if (inputValues.length == 0 || inputValues[0].length == 0) {
-        alert("No Input")
-        return 0
+        inputValues = ["1.0 255 0 0", "0.8 0 255 0", "0.1 0 0 255"]
     }
 
     var total = 0
